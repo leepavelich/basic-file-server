c2e416f345beaced20ad068e5942350140e4d3dd
Lee Pavelich <leepavelich@gmail.com>
2022-01-19 20:25:00 +0000

    Linted

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const conn = net.createConnection({ 
+const conn = net.createConnection({
   host: 'localhost', // change to IP address of computer or ngrok host if tunneling
   port: 3000 // or change to the ngrok port if tunneling
 });
@@ -17,13 +17,13 @@ conn.on('data', data => {
 
 conn.on('connect', () => {
   rl.question(`Please enter the filename you would like to fetch: \n`, answer => {
-    conn.write(`${answer}`)
-    rl.close()
-  })
+    conn.write(`${answer}`);
+    rl.close();
+  });
 });
 
 conn.on('end', () => {
-  console.log('Disconnected from server.')
-})
+  console.log('Disconnected from server.');
+});
 
 conn.setEncoding('utf8'); // interpret data as text
--- server.js
+++ server.js
@@ -7,36 +7,36 @@ server.on('connection', client => {
   console.log('New client connected!');
 
   client.setEncoding('utf8'); // interpret data as text
-  client.on('data', (data) => {
-    console.log('File requested: ', data)
+  client.on('data', data => {
+    console.log('File requested: ', data);
     const path = `./${data}`;
     fs.access(path, fs.F_OK, (err) => {
       if (err) {
-        console.error(path)
-        client.write('File not found')
-        return
+        // console.error(path)
+        client.write('File not found');
+        client.destroy();
       }
     
       //file exists
       fs.readFile(path, 'utf8' , (err, data) => {
         if (err) {
-          console.error(err)
-          return
+          console.error(err);
+          return;
         }
-        client.write(data)
+        client.write(data);
         client.destroy();
-      })
+      });
       
-    })
+    });
   });
 
   client.on('end', () => {
-    console.log('Client closed connection.')
-  })
+    console.log('Client closed connection.');
+  });
 
   client.on('error', err => {
-    console.log(`Error: ${err}`)
-  })
+    console.log(`Error: ${err}`);
+  });
 });
 
 server.listen(3000, () => {
