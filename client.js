bdf26ece0106707902d50ae0b83b2f6900c413af
Lee Pavelich <leepavelich@gmail.com>
2022-01-19 20:18:52 +0000

    Basic implementation completed, console.logs text file lines

--- client.js
+++ client.js
@@ -1,16 +1,29 @@
 const net = require('net');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const conn = net.createConnection({ 
   host: 'localhost', // change to IP address of computer or ngrok host if tunneling
   port: 3000 // or change to the ngrok port if tunneling
 });
 
-conn.on('data', (data) => {
+conn.on('data', data => {
   console.log('Server says: ', data);
 });
 
 conn.on('connect', () => {
-  conn.write('Hello from client!');
+  rl.question(`Please enter the filename you would like to fetch: \n`, answer => {
+    conn.write(`${answer}`)
+    rl.close()
+  })
 });
+
+conn.on('end', () => {
+  console.log('Disconnected from server.')
+})
 
 conn.setEncoding('utf8'); // interpret data as text
--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const net = require('net');
+const fs = require('fs');
 
 const server = net.createServer();
 
-server.on('connection', (client) => {
+server.on('connection', client => {
   console.log('New client connected!');
-  client.write('Hello there!');
 
   client.setEncoding('utf8'); // interpret data as text
   client.on('data', (data) => {
-  console.log('Message from client: ', data)
-});
+    console.log('File requested: ', data)
+    const path = `./${data}`;
+    fs.access(path, fs.F_OK, (err) => {
+      if (err) {
+        console.error(path)
+        client.write('File not found')
+        return
+      }
+    
+      //file exists
+      fs.readFile(path, 'utf8' , (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        client.write(data)
+        client.destroy();
+      })
+      
+    })
+  });
+
+  client.on('end', () => {
+    console.log('Client closed connection.')
+  })
+
+  client.on('error', err => {
+    console.log(`Error: ${err}`)
+  })
 });
 
 server.listen(3000, () => {
